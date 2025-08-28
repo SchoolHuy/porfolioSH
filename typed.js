8c9e37ccaaffa604a17a2491c583217b3700516e
SchoolHuy <huydang78870@gmail.com>
2025-08-28 19:18:08 +0700

    lam about

---
+++ typed.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed-text", {
+  strings: [
+    "Hi, It's <span style='color:#ff004f'>SHUY</span><br>I'm a <span style='color:#00bfff'>Photographer</span>"
+  ],
+  typeSpeed: 70,
+  backSpeed: 40,
+  backDelay: 1500,
+  loop: true,
+  contentType: 'html' // ⬅️ Quan trọng
+});
